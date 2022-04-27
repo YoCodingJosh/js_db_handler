75aa960159074a6ea5c3b655e66c1b0838b44f0b
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2022-04-27 07:30:31 -0500

    some prework

--- helpers/docker.js
+++ helpers/docker.js
@@ -1,5 +1,7 @@
 import { Docker, Options as DockerOptions } from 'docker-cli-js';
 
+const dockerCommand = `docker run --name oshiete-db -e POSTGRES_USER=${process.env.POSTGRES_USER} POSTGRES_PASSWORD=${process.env.PGPASSWORD} -d postgres`;
+
 let startDocker = function() {
 
 };
