9bdff6ae450e0d1219063682c2b87b4210132665
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2022-05-15 13:20:20 -0500

    finally done with migration script generation 👊💥

--- migrations/create-migration.js
+++ migrations/create-migration.js
@@ -14,14 +14,24 @@ const __dirname = path.dirname(__filename);
  */
 export default async function createMigration(name, description) {
     let baseScript = (await readFile(`${__dirname}/__migration_template.js`)).toString();
-    let generationArgs = [name, generateRandomHash(), 'null' /* TODO figure out head migration */, getUsername(), (new Date()).toString(), description];
+    let hash = generateRandomHash();
+    let generationArgs = [name, `'${hash}'`, 'null' /* TODO figure out head migration */, getUsername(), (new Date()).toString(), description];
 
     let tokenizedScript = baseScript.split(' ');
     let interpolatedScriptTokens = [];
     let isFastForwarding = false;
 
-    tokenizedScript.forEach(token => {
+    tokenizedScript.forEach((token, tokenIndex) => {
         let interpolatedToken = token;
+
+        // if we're in dd mode, then ignore all tokens and if a token ends with a new line (\n), then unset fast-forward mode.
+        if (isFastForwarding) {
+            if (token.endsWith('\n')) {
+                isFastForwarding = false;
+            }
+
+            return;
+        }
 
         if (token.startsWith('$$')) {
             let optionalIndex = token.indexOf('?', 2);
@@ -34,9 +44,23 @@ export default async function createMigration(name, description) {
                 if (!generationArgs[actualArgIndexOrdinal]) {
                     // TODO: probably should make it easier to provide different options
                     if (token.includes('dd', optionalIndex)) {
-                        // if it's a dd (vim delete line), just skip it's containing line
-                        // TODO: rewind to last new line (contains \n) and remove each token, and then fast forward to next token after new line (containing \n)
-                        //      this will be a pain in the ass
+                        // if it's a dd (vim delete line), just skip its containing line
+                        // rewind to last new line (contains \n) and remove each token, and then fast forward to the next token after new line (containing \n)
+
+                        // Remove previous tokens on this line.
+                        let i = tokenIndex - 1;
+                        while (true) {
+                            if (interpolatedScriptTokens[i].endsWith('\n') || i < 0) break;
+
+                            interpolatedScriptTokens.splice(i, 1);
+
+                            i--;
+                        }
+
+                        // Ignore tokens until we reach new line.
+                        // Unless.. this token ends with a new line! Then we just ignore like normal lmao.
+                        isFastForwarding = !token.endsWith('\n');
+
                         return;
                     }
                     else {
@@ -47,7 +71,7 @@ export default async function createMigration(name, description) {
             }
 
             if (!generationArgs[actualArgIndexOrdinal]) {
-                throw `Unexpected falsy value for argument index ${argIndexOrdinal} - it should be a string/int passed in`;
+                throw `Unexpected falsy value for argument index ${argIndexOrdinal} - it should be a string/int passed in. maybe interpolate it like this:\n\t\`'\${${generationArgs[actualArgIndexOrdinal]}}'\``;
             } else {
                 interpolatedToken = generationArgs[actualArgIndexOrdinal] + restOfTheToken;
             }
@@ -58,5 +82,5 @@ export default async function createMigration(name, description) {
 
     let interpolatedScript = interpolatedScriptTokens.join(' ');
 
-    console.log(interpolatedScript);
+    await writeFile(`${__dirname}/${hash}-${name}.js`, interpolatedScript);
 }
